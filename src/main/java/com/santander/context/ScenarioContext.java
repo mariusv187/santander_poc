a4917a84be9098f00171d25dfc44b2360f93793b
Marius_Voinea <marius.voinea1221@gmail.com>
2022-05-16 08:48:37 +0300

    add scenario context

---
+++ src/main/java/com/santander/context/ScenarioContext.java
@@ -0,0 +1,26 @@
+package com.santander.context;
+
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@Component
+public class ScenarioContext {
+
+    private ConcurrentMap<String, Object> context = new ConcurrentHashMap<>();
+
+    public <T> void setInContext(String name, Object value) {
+        context.put(name, value);
+    }
+
+    public <T> T getFromContext(String name, Class<T> clazz) {
+        return (T) context.get(name);
+    }
+
+    public void clearContext() {
+        context.clear();
+    }
+}
